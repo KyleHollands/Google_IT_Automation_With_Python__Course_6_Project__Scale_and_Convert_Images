29f79f55f9e740b8a9b13ff918e3cedde193f619
Kyle Hollands <kylehollands@gmail.com>
2020-08-10 17:44:22 -0400

    REASON: Finished implementing user input for origin location and destination for formatted images.
    Working on implementing user input for format
    attributes such as rotation value, extension, scale, etc.

--- Scale_and_Convert.py
+++ Scale_and_Convert.py
@@ -2,17 +2,30 @@ from PIL import Image
 import os
 
 class scale_and_convert:
-    def image_formatting(folder):
-    
+    def __init__(self):
+        pass
+    def image_formatting(self, folder):
+        # Acquire directory information.
         cwd = os.getcwd()
-        
+        dest = cwd + "/" + input("Enter location of formatted files to be saved: ")
+        # Acquire attribute information.
+        rotation = input("Enter image rotation value (clockwise): ")
+        resizing = input("Enter resize values (#,#): ")
+        ext = input("Enter extension to be converted to (.jpg, .png, etc): ")
+                    
+        # If origin location exists, then loop through images to be formatted.
         if os.path.isdir(folder):
-            for image in os.listdir(folder):
-                if image.endswith(".jpg"):
-                    im = Image(image)
-                    im.rotate(90).resize((640,480)).save(cwd/input("Enter destination: "))
+            for img in os.listdir(folder):
+                # if img.endswith(".jpg"):
+                im = Image.open(folder + "/" + img)
+                if os.path.isdir(dest):
+                    im.rotate(90).resize((640,480)).save(dest + "/" + "reformatted_" + img)
+                else:
+                    print("Directory not found.")
+                    break
         else:
-            return False
-           
-    image_formatting(input("Enter file location: "))
-    
+            print("Directory not found.")
+        
+        print("Files formatted successfully.")
+            
+scale_and_convert().image_formatting(input("Enter location of files to be formatted: "))
