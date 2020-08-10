1404d68441b33547ba8d618dfc38c484e177781a
Kyle Hollands <kylehollands@gmail.com>
2020-08-10 19:50:40 -0400

    REASON: Version 1.1 - Finalized the first functional version of the script. It successfully converts over a folder of images to a chosen extension as well as specifying new image dimensions.

--- Scale_and_Convert.py
+++ Scale_and_Convert.py
@@ -1,25 +1,29 @@
 from PIL import Image
 import os
+import re
 
 class scale_and_convert:
     def __init__(self):
         pass
-    def image_formatting(self, folder):
+    def image_formatting(self):
         # Acquire directory information.
         cwd = os.getcwd()
+        origin = cwd + "/" + input("Enter location of formatted files to be formatted: ")
         dest = cwd + "/" + input("Enter location of formatted files to be saved: ")
+        
         # Acquire attribute information.
-        rotation = input("Enter image rotation value (clockwise): ")
-        resizing = input("Enter resize values (#,#): ")
+        rotation = int(input("Enter image rotation value (clockwise): "))
+        img_width = int(input("Enter new image width: "))
+        img_height = int(input("Enter new image height: "))
         ext = input("Enter extension to be converted to (.jpg, .png, etc): ")
-                    
+
         # If origin location exists, then loop through images to be formatted.
-        if os.path.isdir(folder):
-            for img in os.listdir(folder):
-                # if img.endswith(".jpg"):
-                im = Image.open(folder + "/" + img)
+        if os.path.isdir(origin):
+            for img in os.listdir(origin):
                 if os.path.isdir(dest):
-                    im.rotate(90).resize((640,480)).save(dest + "/" + "reformatted_" + img)
+                    im = Image.open(origin + "/" + img)
+                    img_ext = re.sub(r'([.][a-z]+)',ext,img)
+                    im.rotate(rotation).resize((img_width,img_height)).save(dest + "/" + "reformatted_" + img_ext)
                 else:
                     print("Directory not found.")
                     break
@@ -28,4 +32,4 @@ class scale_and_convert:
         
         print("Files formatted successfully.")
             
-scale_and_convert().image_formatting(input("Enter location of files to be formatted: "))
+scale_and_convert().image_formatting()
