0ad0699039931361d05fb275a4a9694c7a0e7bdd
Kyle Hollands <kylehollands@gmail.com>
2020-08-11 18:14:59 -0400

    REASON: v1.2 of the Scale_and_Convert script. Acquries location of original files and destination of converted files. Asks user how much to rotate the image, the width and height of the image and the image extension.
    Currently works fine in Windows, but having issues getting it to
    function properly in linux.

--- Scale_and_Convert.py
+++ Scale_and_Convert.py
@@ -1,4 +1,6 @@
-from PIL import Image
+#!/usr/bin/env python3
+
+from PIL import Image, UnidentifiedImageError
 import os
 import re
 
@@ -8,28 +10,35 @@ class scale_and_convert:
     def image_formatting(self):
         # Acquire directory information.
         cwd = os.getcwd()
+        
         origin = cwd + "/" + input("Enter location of formatted files to be formatted: ")
         dest = cwd + "/" + input("Enter location of formatted files to be saved: ")
+        
+        # origin = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 1\Resources\Scale and Convert Images using PIL\Incorrectly Formatted Images/"
+        # dest = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 1\Resources\Scale and Convert Images using PIL\Correctly Formatted Images/"
+        
+        # origin = "/home/student-04-af18cecbf8f1/images/"
+        # dest = "/opt/icons/"
         
         # Acquire attribute information.
         rotation = int(input("Enter image rotation value (clockwise): "))
         img_width = int(input("Enter new image width: "))
         img_height = int(input("Enter new image height: "))
-        ext = input("Enter extension to be converted to (.jpg, .png, etc): ")
+        ext = str(input("Enter extension to be converted to (.jpg, .png, etc): "))
 
         # If origin location exists, then loop through images to be formatted.
-        if os.path.isdir(origin):
-            for img in os.listdir(origin):
+        
+        for img in os.listdir(origin):
+            try:
                 if os.path.isdir(dest):
                     im = Image.open(origin + "/" + img)
-                    img_ext = re.sub(r'([.][a-z]+)',ext,img)
-                    im.rotate(rotation).resize((img_width,img_height)).save(dest + "/" + "reformatted_" + img_ext)
-                else:
-                    print("Directory not found.")
-                    break
-        else:
-            print("Directory not found.")
-        
+                    img = re.sub(r'([.][a-z]+)',ext,img)
+                    im.rotate(rotation).resize((img_width,img_height)).convert("RGB").save(dest + "/" + img)
+                    im.close()
+
+            except UnidentifiedImageError:
+                continue
+            
         print("Files formatted successfully.")
             
 scale_and_convert().image_formatting()
