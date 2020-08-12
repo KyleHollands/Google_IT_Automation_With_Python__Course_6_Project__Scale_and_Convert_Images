73e635b9bffa7309d1be949719f97a1ff223262a
Kyle Hollands <kylehollands@gmail.com>
2020-08-12 12:59:33 -0400

    REASON: v1.4 - Final version (for now). Haven't fully tested recent modifications in a linux environment, but a few modifications can be made to make it work for sure.

--- Scale_and_Convert.py
+++ Scale_and_Convert.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-from PIL import Image, UnidentifiedImageError
-from sys import platform
+from PIL import Image
 import os
-import re
 
 class scale_and_convert:
     def __init__(self):
@@ -11,18 +9,8 @@ class scale_and_convert:
     def image_formatting(self):
         # Acquire directory information.
         cwd = os.getcwd()
-        
-        # origin = cwd + "/" + input("Enter location of formatted files to be formatted: ")
-        # dest = cwd + "/" + input("Enter location of formatted files to be saved: ")
-        
-        # origin = input("Enter location of formatted files to be formatted: ")
-        # dest = input("Enter location of formatted files to be saved: ")
-        
-        # origin = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 1\Resources\Scale and Convert Images using PIL\Incorrectly Formatted Images/"
-        # dest = r"C:\Users\KyleHollands\OneDrive\Education\Coursera\Google IT Automation with Python\Automating Real-World Tasks with Python\Week 1\Resources\Scale and Convert Images using PIL\Correctly Formatted Images/"
-        
-        origin = "/home/student-02-55178bc2d8eb/images/"
-        dest = "/opt/icons/"
+        origin = cwd + "/" + input("Enter location of formatted files to be formatted: ")
+        dest = cwd + "/" + input("Enter location of formatted files to be saved: ")
         
         # Acquire attribute information.
         rotation = int(input("Enter image rotation value (clockwise): "))
@@ -30,30 +18,21 @@ class scale_and_convert:
         img_height = int(input("Enter new image height: "))
         ext = str(input("Enter extension to be converted to (.jpg, .png, etc): "))
         
-        # Loop through all images in acquired directory, then apply modifications.
-        
-        # Tested to work in linux
-        
+        # Loop through images in chosen directory, then format according to information provided.
         for image in os.listdir(origin):
             try:
-                img = Image.open(origin + image)
-                img.rotate(rotation).resize((img_width,img_height)).convert("RGB").save(dest + image.split('.')[0],ext)
+                img = Image.open(origin + "/" + image)
+                img.rotate(rotation).resize((img_width,img_height)).convert("RGB").save(dest + "/" + image.split('.')[0] + "." + ext)
                 img.close()
                 
-            except UnidentifiedImageError:
+            # If a non-image file is located, skip and hide error message.
+            except OSError:
                 continue
-        
-        # Tested to work in Windows
-        
-        # for img in os.listdir(origin):
-        #     try:
-        #         if os.path.isdir(dest):
-        #             im = Image.open(origin + "/" + img)
-        #             img = re.sub(r'([.][a-z]+)',ext,img)
-        #             im.rotate(rotation).resize((img_width,img_height)).convert("RGB").save(dest + "/" + img)
-        #             im.close()
-
-        #     except UnidentifiedImageError:
-        #         continue
             
+        print("Files formatted successfully.")
+        
+        # Prevent window from closing immediately.
+        
+        k=input("press close to exit") 
+        
 scale_and_convert().image_formatting()
